78cab14ae6478af49d14a6d16b715831506d107d
Sebastian Johnsson <sebastian@agiley.se>
2012-08-31 22:03:04 +0200

    Add additional template variables. - Possible to configure the web server options - Configure include paths

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,18 +1,23 @@
-default[:monit][:notify_email]          = "notify@example.com"
+default[:monit][:notify_email]                      =   "notify@example.com"
 
-default[:monit][:poll_period]           = 60
-default[:monit][:poll_start_delay]      = 120
+default[:monit][:poll_period]                       =   60
+default[:monit][:poll_start_delay]                  =   120
 
-default[:monit][:mail_format][:subject] = "$SERVICE $EVENT"
-default[:monit][:mail_format][:from]    = "monit@example.com"
-default[:monit][:mail_format][:message]    = <<-EOS
+default[:monit][:mail_format][:subject]             =   "$SERVICE $EVENT"
+default[:monit][:mail_format][:from]                =   "monit@example.com"
+default[:monit][:mail_format][:message]             =   <<-EOS
 Monit $ACTION $SERVICE at $DATE on $HOST: $DESCRIPTION.
 Yours sincerely,
 monit
 EOS
 
-default[:monit][:mailserver][:host] = "localhost"
-default[:monit][:mailserver][:port] = nil
-default[:monit][:mailserver][:username] = nil
-default[:monit][:mailserver][:password] = nil
-default[:monit][:mailserver][:password_suffix] = nil
+default[:monit][:mailserver][:host]                 =   "localhost"
+default[:monit][:mailserver][:port]                 =   nil
+default[:monit][:mailserver][:username]             =   nil
+default[:monit][:mailserver][:password]             =   nil
+default[:monit][:mailserver][:password_suffix]      =   nil
+
+default[:monit][:webserver][:port]                  =   2812
+default[:monit][:webserver][:bind_to_localhost]     =   true
+default[:monit][:webserver][:allowed_connections]   =   ["localhost"]
+default[:monit][:webserver][:include_paths]         =   ["/etc/monit/conf.d/*.conf"]
