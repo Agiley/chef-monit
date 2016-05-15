a94b775cba32d79470816cb9a904488052d933d0
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2016-05-15 22:17:45 +0700

    Add support for systemd

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,40 +11,48 @@ cookbook_file "/etc/default/monit" do
   only_if { platform?("ubuntu") }
 end
 
-if (node['monit']['log'] && node['monit']['log'].include?('/'))
-  folder = File.dirname(node['monit']['log'])
-  
-  if (folder)
-    bash "create_log_directory" do
-      code <<-EOH
-        mkdir -p #{folder}
-        chown -R #{node['monit']['source']['user']}:#{node['monit']['source']['group']} #{folder}
-      EOH
-    end
-  end
+bash "create_log_directory" do
+  code <<-EOH
+    mkdir -p #{File.dirname(node['monit']['log'])}
+    chown -R #{node['monit']['source']['user']}:#{node['monit']['source']['group']} #{File.dirname(node['monit']['log'])}
+  EOH
+  only_if { node['monit']['log'] && node['monit']['log'].include?('/')}
 end
 
-service "monit" do
-  action :enable
-  supports [:start, :restart, :stop]
+template "/etc/systemd/system/monit.service" do
+  source 'monit.systemd.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 00744
+  notifies :run, 'execute[systemctl daemon-reload]', :immediately
+  only_if { platform?('ubuntu') && Chef::VersionConstraint.new('>= 15.04').include?(node['platform_version']) }
 end
+
+execute 'systemctl daemon-reload' do
+  action :nothing
+end
+
+node[:monit][:include_paths].each do |include_path|
+  directory include_path do
+    owner  'root'
+    group 'root'
+    mode 0755
+    action :create
+    recursive true
+    not_if { ::File.exists?(include_path) }
+  end
+end if node[:monit][:include_paths] && node[:monit][:include_paths].any?
 
 template "/etc/monit/monitrc" do
   owner "root"
   group "root"
   mode 0700
   source 'monitrc.erb'
-  notifies :restart, resources(:service => "monit"), :delayed
-end
-
-directory "/etc/monit/conf.d/" do
-  owner  'root'
-  group 'root'
-  mode 0755
-  action :create
-  recursive true
+  notifies :restart, resources(:service => "monit"), :immediately
 end
 
 service "monit" do
-  action :start
+  action :enable
+  supports [:start, :restart, :stop]
+  service_provider = platform?('ubuntu') ? find_provider : nil
 end
