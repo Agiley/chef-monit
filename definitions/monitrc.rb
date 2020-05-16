4f995a9fcb3b7a044571646c2e1199f1c924e3ef
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-05-16 15:07:15 +0700

    Create config files in conf-available, symlink to conf-enabled

--- definitions/monitrc.rb
+++ definitions/monitrc.rb
@@ -5,21 +5,31 @@
 # template_source: filename of the ERB configuration template, defaults to <LWRP Name>.conf.erb
 define :monitrc, action: :enable, reload: :delayed, variables: {}, template_cookbook: "monit", template_source: nil do
   params[:template_source] ||= "#{params[:name]}.conf.erb"
-  if params[:action] == :enable
-    template "/etc/monit/conf.d/#{params[:name]}.conf" do
+  
+  if params[:action] == :enable  
+    template "/etc/monit/conf-available/#{params[:name]}.conf" do
       owner "root"
       group "root"
       mode 0644
       source params[:template_source]
       cookbook params[:template_cookbook]
       variables params[:variables]
-      notifies :restart, resources(service: "monit"), params[:reload]
       action :create
     end
-  else
-    template "/etc/monit/conf.d/#{params[:name]}.conf" do
-      action :delete
+    
+    link "/etc/monit/conf-available/#{params[:name]}.conf" do
+      to "/etc/monit/conf-enabled/#{params[:name]}.conf"
       notifies :restart, resources(service: "monit"), params[:reload]
     end
+    
+  else
+    paths = ["/etc/monit/conf-available/#{params[:name]}.conf", "/etc/monit/conf-enabled/#{params[:name]}.conf"]
+    paths.each_with_index do |path, index|
+      template "/etc/monit/conf.d/#{params[:name]}.conf" do
+        action :delete
+        notifies :restart, resources(service: "monit"), params[:reload]
+      end
+    end
+    
   end
 end
